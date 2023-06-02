3f55937eba8860c365eb0dcb14b0261c5e164f41
S1M0N38 <bertolottosimone@gmail.com>
2023-06-02 20:10:08 +0200

    Add AlignmentAccuracy in metrics.py
    AlignmentAccuracy is a metrics that must be used to calculate accuracy
    for models that returns embeddings as outputs. If the model returns
    logits or probabilities you can take the argmax to get the predictions
    labels, but if the model return an embedding you must compare it to the
    class embeddings and select the top_k similar embeddings (we use
    cosine similarity as similarity score).

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -1,11 +1,30 @@
+import numpy as np
+import torch
 import torchmetrics
 from torchmetrics import classification
 
 Accuracy = torchmetrics.Accuracy
-MulticlassAccuracy = classification.MulticlassAccuracy
 
 
 class EmbeddingsAccuracy(classification.MulticlassAccuracy):
-    def update(self, outputs, targets):
+    def update(self, outputs: torch.Tensor, targets: torch.Tensor):
         targets = targets.argmax(dim=-1)
         super().update(outputs, targets)
+
+
+class AlignmentAccuracy(torchmetrics.Metric):
+    def __init__(self, path_embeddings: str, top_k: int = 1) -> None:
+        super().__init__()
+        self.embeddings = torch.from_numpy(np.load(path_embeddings)).to("cuda")
+        self.top_k = top_k
+        self.add_state("correct", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("total", default=torch.tensor(0), dist_reduce_fx="sum")
+
+    def update(self, outputs: torch.Tensor, targets: torch.Tensor):
+        _, preds = (outputs @ self.embeddings.T).topk(self.top_k)
+        labels = (targets @ self.embeddings.T).argmax(dim=-1)
+        self.correct += (preds == labels.unsqueeze(-1)).any(dim=-1).sum()
+        self.total += labels.numel()  # type: ignore
+
+    def compute(self) -> torch.Tensor:
+        return self.correct / self.total  # type: ignore
