6c1b66614b982814a5577fcc425626023a731006
S1M0N38 <bertolottosimone@gmail.com>
2023-07-11 09:40:45 +0200

    Fix bug AlignmentAccuracy: need to normalize encodings

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import torch.nn.functional as F
 import torchmetrics
 from torchmetrics import classification
 
@@ -8,19 +9,23 @@ Accuracy = torchmetrics.Accuracy
 
 class EmbeddingsAccuracy(classification.MulticlassAccuracy):
     def update(self, outputs: torch.Tensor, targets: torch.Tensor):
-        targets = targets.argmax(dim=-1)
-        super().update(outputs, targets)
+        labels = targets.argmax(dim=-1)
+        super().update(outputs, labels)
 
 
 class AlignmentAccuracy(torchmetrics.Metric):
     def __init__(self, path_embeddings: str, top_k: int = 1) -> None:
         super().__init__()
-        self.embeddings = torch.from_numpy(np.load(path_embeddings)).to("cuda")
+        embeddings = np.load(path_embeddings)
+        embeddings /= np.linalg.norm(embeddings, axis=1, keepdims=True)
+        self.embeddings = torch.from_numpy(embeddings).to("cuda")
         self.top_k = top_k
         self.add_state("correct", default=torch.tensor(0), dist_reduce_fx="sum")
         self.add_state("total", default=torch.tensor(0), dist_reduce_fx="sum")
 
     def update(self, outputs: torch.Tensor, targets: torch.Tensor):
+        outputs = F.normalize(outputs, p=2, dim=1)
+        targets = F.normalize(targets, p=2, dim=1)
         _, preds = (outputs @ self.embeddings.T).topk(self.top_k)
         labels = (targets @ self.embeddings.T).argmax(dim=-1)
         self.correct += (preds == labels.unsqueeze(-1)).any(dim=-1).sum()
